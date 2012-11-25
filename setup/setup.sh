a571df6ef74ac0a4bf4c13ad867b816b351fa46a
Gareth G. Aye <gareth.aye@gmail.com>
2012-11-25 10:10:54 -0800

    Add stuff to move nginx config to default loc

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,6 +5,7 @@ ROOTDIR="/home/ubuntu/garethaye"
 
 `"$ROOTDIR/git/install.sh"`
 `"$ROOTDIR/nginx/install.sh"`
+sudo cp "$ROOTDIR/nginx/default /etc/nginx/sites-available"
 
 su ubuntu
 
@@ -14,3 +15,4 @@ cp "$ROOTDIR/config/.vimrc" ~
 # Make a public directory to put nginx in front of
 cd ~
 mkdir -p public
+sudo chmod a+rx public
